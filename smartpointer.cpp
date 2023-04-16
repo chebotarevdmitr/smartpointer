9506878cff49a88b17fae43a01d1a8ca3fba1d4b
DIM <4ebotarov_dim@inbox.ru>
2023-04-16 22:00:03 +0300

    ownership unPtr2

--- smartpointer.cpp
+++ smartpointer.cpp
@@ -6,9 +6,9 @@ using namespace std;
 int main()
 {
 	unique_ptr<int>unPtr1 = make_unique<int>(25);
-	cout << unPtr1 << endl;
-	cout << *unPtr1 << endl;
-
+	//cout << unPtr1 << endl;
+	//cout << *unPtr1 << endl;
+	unique_ptr<int>unPtr2 = move(unPtr1);
 
 	system("pause>0");
 	return 0;
