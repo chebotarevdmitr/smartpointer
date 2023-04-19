2c021258c6b13736c3d5576fa813b9272d6476b1
chebotarevdmitr <127180093+chebotarevdmitr@users.noreply.github.com>
2023-04-19 23:06:33 +0300

    закомит ненужн

--- smartpointer.cpp
+++ smartpointer.cpp
@@ -17,12 +17,12 @@ public:
 
 int main()
 {
-	unique_ptr<int>unPtr1 = make_unique<int>(25);
-	//cout << unPtr1 << endl;
-	//cout << *unPtr1 << endl;
+	/*unique_ptr<int>unPtr1 = make_unique<int>(25);
+	//1comit cout << unPtr1 << endl;
+	//1comit cout << *unPtr1 << endl;
 	unique_ptr<int>unPtr2 = move(unPtr1);
 	cout << *unPtr2 << endl;
-
+	*/
 	 
 	system("pause>0");
 	return 0;
